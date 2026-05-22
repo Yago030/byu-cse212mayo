c19e72fa7037286a51809a131703499874c36531
Santiago Bergerat <santiagobergerat@gmail.com>
2026-05-21 23:18:36 -0300

    Complete W03 sets and maps assignment

--- week03/learn/Translator.cs
+++ week03/learn/Translator.cs
@@ -6,6 +6,7 @@ public class Translator
         englishToGerman.AddWord("House", "Haus");
         englishToGerman.AddWord("Car", "Auto");
         englishToGerman.AddWord("Plane", "Flugzeug");
+
         Console.WriteLine(englishToGerman.Translate("Car")); // Auto
         Console.WriteLine(englishToGerman.Translate("Plane")); // Flugzeug
         Console.WriteLine(englishToGerman.Translate("Train")); // ???
@@ -14,27 +15,23 @@ public class Translator
     private Dictionary<string, string> _words = new();
 
     /// <summary>
-    /// Add the translation from 'from_word' to 'to_word'
-    /// For example, in a english to german dictionary:
-    /// 
-    /// my_translator.AddWord("book","buch")
+    /// Add the translation from 'fromWord' to 'toWord'
     /// </summary>
-    /// <param name="fromWord">The word to translate from</param>
-    /// <param name="toWord">The word to translate to</param>
-    /// <returns>fixed array of divisors</returns>
     public void AddWord(string fromWord, string toWord)
     {
-        // ADD YOUR CODE HERE
+        _words[fromWord] = toWord;
     }
 
     /// <summary>
-    /// Translates the from word into the word that this stores as the translation
+    /// Translates the from word into the stored translation
     /// </summary>
-    /// <param name="fromWord">The word to translate</param>
-    /// <returns>The translated word or "???" if no translation is available</returns>
     public string Translate(string fromWord)
     {
-        // ADD YOUR CODE HERE
-        return "";
+        if (_words.ContainsKey(fromWord))
+        {
+            return _words[fromWord];
+        }
+
+        return "???";
     }
 }
